8003ab2f3003e613a91feb347c8aaff8db92399a
Daniele Sergio <daniele.sergio@kynetics.com>
2022-09-15 16:36:39 +0200

    Implement communication api v1

---
+++ uf-client-service/src/main/kotlin/com/kynetics/uf/android/communication/CommunicationApiV1Impl.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2017-2022  Kynetics  LLC
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package com.kynetics.uf.android.communication
+
+import com.kynetics.uf.android.api.ApiCommunicationVersion
+import com.kynetics.uf.android.client.RestartableClientService
+import com.kynetics.uf.android.configuration.AndroidDeploymentPermitProvider
+import com.kynetics.uf.android.configuration.ConfigurationHandler
+
+class CommunicationApiV1Impl(configurationHandler: ConfigurationHandler,
+                             ufService: RestartableClientService,
+                             softDeploymentPermitProvider: AndroidDeploymentPermitProvider
+):
+    AbstractCommunicationApi(configurationHandler, ufService, softDeploymentPermitProvider), CommunicationApiV1{
+    override val api: ApiCommunicationVersion = ApiCommunicationVersion.V1
+}
