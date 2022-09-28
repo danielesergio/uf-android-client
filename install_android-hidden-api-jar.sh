e96e97f8de739b58d16acc32c7d5ca63d7d47f9b
Daniele Sergio <daniele.sergio@kynetics.com>
2022-09-28 15:35:56 +0200

    Extend copyright to 2022

--- install_android-hidden-api-jar.sh
+++ install_android-hidden-api-jar.sh
@@ -1,13 +1,11 @@
 #
-# Copyright © 2022  Kynetics  LLC
+# Copyright © 2017-2022  Kynetics  LLC
 #
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
 #
-# SPDX-License-Identifier: EPL-2.0
-#
-#!/bin/sh
 
 if [ -z "${ANDROID_HOME}" ] ; then
 	echo "ANDROID:HOME is not set. Please set it and try again"
